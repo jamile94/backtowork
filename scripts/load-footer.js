1b97ee7820179a0f2988e2c6cf367af5f50f420a
Lucas Perez <lucastperez@gmail.com>
2020-11-27 18:33:52 -0300

    Instruções para editar o script

--- scripts/load-footer.js
+++ scripts/load-footer.js
@@ -1,3 +1,28 @@
+/*                          INSTRUÇÕES PARA EDITAR                           */
+/*****************************************************************************/
+/* Para alterar o HTML do header e do footer você deve alterar o HTML que    */
+/* está dentro da função document.write('...')                               */
+/*                                                                           */
+/* Toda vez que for começar uma linha nova, tem que colocar uma              */
+/* barrinha invertida: \                                                     */
+/*                                                                           */
+/*   Não vai funcionar:                                                      */
+/*      document.write(' <h1>Título</h1>                                     */
+/*                       <p>Parágrafo</p> ');                                */
+/*   Vai funcionar:                                                          */
+/*      document.write(' <h1>Título</h1>\                                    */
+/*                       <p>Parágrafo</p> ');                                */
+/*                                                                           */
+/* Quando for usar aspas, use aspas duplas "", pois a função document.write  */
+/* já recebe uma string que está usando aspas simples                        */
+/*                                                                           */
+/*   Não vai funcionar: document.write(' <div class='classe'>Oi</div> ');    */
+/*                                                                           */
+/*   Vai funcionar:     document.write(' <div class="classe">Oi</div> ');    */
+/*                                                                           */
+/* Observe o que já está feito neste arquivo como exemplo.                   */
+/*****************************************************************************/
+
 document.write('\
 Telefone:(55) 12344 - 5678 </br>\
 Email: contato@backtowork </br>\
