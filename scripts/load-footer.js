34dc514180626046a871aacbb5536f55ab67e5a8
Lucas Perez <lucastperez@gmail.com>
2020-11-27 18:13:07 -0300

    Scripts com html do header e do footer

---
+++ scripts/load-footer.js
@@ -0,0 +1,25 @@
+document.write('\
+Telefone:(55) 12344 - 5678 </br>\
+Email: contato@backtowork </br>\
+<span class="nets">\
+  Redes sociais:</br>\
+</span>\
+\
+<div class="redes">\
+  <span>\
+    <a href="">\
+      <img src="./images/Facebook.png" alt="Facebook">\
+    </a>\
+  </span>\
+  <span>\
+    <a href="">\
+      <img src="./images/twitter.png" alt="Twitter">\
+    </a>\
+  </span>\
+  <span>\
+    <a href="">\
+      <img src="./images/youtube.png" alt="Youtube">\
+    </a>\
+  </span>\
+</div>\
+');
---
+++ scripts/load-header.js
@@ -0,0 +1,23 @@
+document.write('\
+<a href="index.html">\
+  <div class="logo">\
+    <div id="triangle"></div>\
+    <div id="backtowork">BACK TO WORK</div>\
+  </div>\
+</a>\
+<button class="btn-menu">\
+  <i class="fas fa-bars fa-lg"></i>\
+</button>\
+<nav class="menu">\
+  <a href="#" class="btn-close">x</a>\
+  <ul>\
+    <a href=#><li>O projeto</li></a>\
+    <a href="about.html"><li>Quem somos</li></a>\
+    <a href="cursos.html"><li>Cursos</li></a>\
+    <a href="contatos.html"><li>Contatos</li></a>\
+  </ul>\
+</nav>\
+<div id="backtriangle"></div>\
+\
+<script src="./scripts/menu.js"></script>\
+');
