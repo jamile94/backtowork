c612a8ef7e60990835811f8a27dce8616e3930b6
Marcelo Souza <locee_rj@hotmail.com>
2020-11-28 18:23:42 -0300

    feito lógica pra prencher a lista de aulas dinamicamente

---
+++ scripts/modulo-videos.js
@@ -0,0 +1,25 @@
+function elements(){
+    let div = $(".video-list");
+    let panel = $(".panel")
+    let iframe = `<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/T5mzxYpp3WQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    videos.forEach((video) => {
+        div.append(`<button class="col-sm-12 col-md-12 checked" id="${video.id}">${video.name}</button>`);
+        
+    });
+    panel.append(iframe);
+
+    let button = $(".video-list button");
+    console.log(button);
+    let video = $("iframe")
+    for(var el of button){
+        console.log(el)
+        el.addEventListener("click", (e) => {
+            console.log(e.target.id)
+            panel.html(`<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${e.target.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+        })
+    }
+    
+    
+}
+
+elements();
