b8c1bcdde600f6ae25c44bf73831730b11d49c05
Lucas Perez <lucastperez@gmail.com>
2020-11-29 14:39:00 -0300

    WIP: mudando dinâmicamente a página de módulo

--- scripts/modulos-e-videos/load-videos.js
+++ scripts/modulos-e-videos/load-videos.js
@@ -1,10 +1,14 @@
 function elements(){
+    let moduleNumber = $(".module-numner").text();
     let div = $(".video-list");
     let panel = $(".panel")
     let iframe = `<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/T5mzxYpp3WQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
     videos.forEach((video) => {
         div.append(`<button id="${video.id}">${video.name}</button>`);
     });
+
+    console.log("oi\n");
+    console.log(moduleNumber);
 
     /* Coloca o título do primeiro vídeo na tela*/
     $(".video-list").before(`<div class="green col-sm-12 col-md-12 col-lg-12 video-name">
