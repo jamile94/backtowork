9e9e2a7a5d9b2713b882524bfb0c0dada806b53b
Lucas Perez <lucastperez@gmail.com>
2020-11-29 02:08:11 -0300

    Mostrar título dos vídeos no topo da tela de Módulos X

--- scripts/modulos-e-videos/check-button-video.js
+++ scripts/modulos-e-videos/check-button-video.js
@@ -4,6 +4,9 @@ function checkButton() {
     $('.checked').removeClass('checked');
     $(this).addClass('checked');
     $(this).css('background-color', 'rgb(54, 130, 118, 0.7)');
+    $(".video-name").remove();
+    $(".video-list").before(`<div class="green col-sm-12 col-md-12 col-lg-12 video-name">
+                            ${videos.find(x => x.id == $(this).attr('id')).name}</div>`);
   })
 };
 
--- scripts/modulos-e-videos/load-videos.js
+++ scripts/modulos-e-videos/load-videos.js
@@ -6,18 +6,20 @@ function elements(){
         div.append(`<button id="${video.id}">${video.name}</button>`);
     });
 
+    /* Coloca o título do primeiro vídeo na tela*/
+    $(".video-list").before(`<div class="green col-sm-12 col-md-12 col-lg-12 video-name">
+                            ${videos[0].name}</div>`)
     panel.append(iframe);
 
     let button = $(".video-list button");
-    // console.log(button);
     let video = $("iframe")
     for(var el of button){
-        // console.log(el)
         el.addEventListener("click", (e) => {
-            // console.log(e.target.id)
             panel.html(`<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${e.target.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
         })
     }
 };
+
+// function formatVideoName(string) { return string.split(' - ').splice(1).join(' ') }
 
 elements();
