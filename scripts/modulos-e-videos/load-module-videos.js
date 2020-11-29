ae8352a48d9db8c23931c7f4338847e99b4e562c
Lucas Perez <lucastperez@gmail.com>
2020-11-29 19:27:03 -0300

    Refatoração de JS

--- scripts/modulos-e-videos/load-module-videos.js
+++ scripts/modulos-e-videos/load-module-videos.js
@@ -3,24 +3,29 @@ function loadModuleVideos(){
     console.log(moduleNumber);
 
     let videolist = $(".video-list");
-    let panel = $(".video")
-    let firstVideo = videos[moduleNumber][0] // Pega o primeiro vídeo da lista
+    let panel = $(".video");
+    let firstVideo = videos[moduleNumber][0]; // Pega o primeiro vídeo da lista
     let iframe = `<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${firstVideo.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+
+    /* Coloca o título do primeiro vídeo na tela */
+    $(".video-name").text(`${firstVideo.name}`);
 
     /* Coloca a lista com os botões para cada vídeo na div .video-list */
     videos[moduleNumber].forEach((video) => {
-        videolist.append(`<button id="${video.id}">${video.name}</button>`);
+        videolist.append(`<button class="video-button" id="${video.id}">${video.name}</button>`);
     });
-
-    /* Coloca o título do primeiro vídeo na tela*/
-    $(".video-name").text(`${videos[moduleNumber][0].name}`);
 
     panel.append(iframe);
 
     let button = $(".video-list button");
-    let video = $("iframe")
+    let video = $("iframe");
     for(var el of button){
         el.addEventListener("click", (e) => {
+            // console.log(e);
+            // console.log(typeof(e));
+            // console.log(this);
+            // console.log(e.target);
+            // console.log(e.target.id);
             panel.html(`<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${e.target.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
         })
     }
