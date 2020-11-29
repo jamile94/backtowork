2fcd330191d2d1b42ba4a60d7d6f922228a8d029
Lucas Perez <lucastperez@gmail.com>
2020-11-29 00:30:46 -0300

    Estilizando botões um pouco mais

--- scripts/modulos-e-videos/check-button-video.js
+++ scripts/modulos-e-videos/check-button-video.js
@@ -1,9 +1,9 @@
 function checkButton() {
   $('.video-list button').click(function() {
-    $('.checked').css('background-color','white')
+    $('.checked').css('background-color', '')
     $('.checked').removeClass('checked');
     $(this).addClass('checked');
-    $(this).css('background-color', '#347d6c');
+    $(this).css('background-color', 'rgb(54, 130, 118, 0.7)');
   })
 };
 
