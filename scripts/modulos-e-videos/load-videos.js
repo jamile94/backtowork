3dfea0008fe90f6ec80946f022c8439bc5866d29
Marcelo Souza <locee_rj@hotmail.com>
2020-11-30 19:56:22 -0300

    scrolando ao apertar o botão de vídeo

--- scripts/modulos-e-videos/load-videos.js
+++ scripts/modulos-e-videos/load-videos.js
@@ -1,7 +1,7 @@
 function elements(){
     let div = $(".video-list");
-    let panel = $(".panel")
-    let iframe = `<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/T5mzxYpp3WQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    let panel = $(".panel");
+    let iframe = `<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/T5mzxYpp3WQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
     videos.forEach((video) => {
         div.append(`<button id="${video.id}">${video.name}</button>`);
     });
@@ -12,13 +12,28 @@ function elements(){
     panel.append(iframe);
 
     let button = $(".video-list button");
-    let video = $("iframe")
+    
     for(var el of button){
         el.addEventListener("click", (e) => {
-            panel.html(`<iframe class="col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${e.target.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+            panel.html(`<iframe class="video col-sm-12 col-md-12 col-lg-12" src="https://www.youtube.com/embed/${e.target.id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+            scrolling(e);
+            getVideoDuration()
+            
         })
+
+       
     }
+    
 };
+
+function getVideoDuration(){
+    let video = $(".video");
+    console.log(video);
+}
+
+function scrolling(e){
+    e.target.scrollIntoView();
+}
 
 // function formatVideoName(string) { return string.split(' - ').splice(1).join(' ') }
 
